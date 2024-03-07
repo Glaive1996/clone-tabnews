edeea395972aa6994d275785145d92f7a446b6a9
Patrick Flores <67614684+Glaive1996@users.noreply.github.com>
2024-03-07 23:37:25 +0000

    modificar frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Oi amor, se voce me ama da uma risadinha</h1>
+    return <h1>Oi amor, finalmete vamos ter um bebê, estou muito feliz</h1>
 }
 
 export default Home;
