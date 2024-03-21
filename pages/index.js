ec81adb8bcc771ab9ce646d23afed8c969f326c5
Patrick Flores <67614684+Glaive1996@users.noreply.github.com>
2024-03-21 00:20:15 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-    return <h1>Oi amor, eu te amo, se você também me ama da uma risadinha!</h1>
+function Home() {
+  return <h1>Oi amor, eu te amo, se você também me ama da uma risadinha!</h1>;
 }
 
 export default Home;
